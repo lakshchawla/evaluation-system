dd2074896b48eb60185940780aeb8f9fca95a683
Lakshay Chawla <lakshaychawla6677@gmail.com>
2022-01-30 22:38:49 +0530

    First Release

--- server.js
+++ server.js
@@ -9,16 +9,15 @@ const req = require('express/lib/request');
 const app = express();
 
 mongoose.connect(
-	"mongodb+srv://lakshay:lakshay@cluster0.as40i.mongodb.net/job-application-database",
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	}
+    "mongodb+srv://lakshay:lakshay@cluster0.as40i.mongodb.net/job-application-database", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
 );
 
 const userLoginSchema = new mongoose.Schema({
-	username: String,
-	password: String,
+    username: String,
+    password: String,
 });
 
 const admins = mongoose.model('admins', userLoginSchema);
@@ -27,9 +26,9 @@ const admins = mongoose.model('admins', userLoginSchema);
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(session({
-	secret: "verygoodsecret",
-	resave: false,
-	saveUninitialized: true
+    secret: "verygoodsecret",
+    resave: false,
+    saveUninitialized: true
 }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -38,82 +37,82 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser(function (user, done) {
-	done(null, user.id);
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
 });
 
-passport.deserializeUser(function (id, done) {
-	admins.findById(id, function (err, user) {
-		done(err, user);
-	});
+passport.deserializeUser(function(id, done) {
+    admins.findById(id, function(err, user) {
+        done(err, user);
+    });
 });
 
-passport.use(new localStrategy(function (username, password, done) {
-	admins.findOne({ username: username }, function (err, user) {
-		if (err) return done(err);
-		if (!user) return done(null, false, { message: 'Incorrect username.' });
+passport.use(new localStrategy(function(username, password, done) {
+    admins.findOne({ username: username }, function(err, user) {
+        if (err) return done(err);
+        if (!user) return done(null, false, { message: 'Incorrect username.' });
 
-		bcrypt.compare(password, user.password, function (err, res) {
-			if (err) return done(err);
-			if (res === false) return done(null, false, { message: 'Incorrect password.' }, console.log("Incorrect Password"));
+        bcrypt.compare(password, user.password, function(err, res) {
+            if (err) return done(err);
+            if (res === false) return done(null, false, { message: 'Incorrect password.' }, console.log("Incorrect Password"));
 
-			return done(null, user);
-		});
-	});
+            return done(null, user);
+        });
+    });
 }));
 
 function isLoggedIn(req, res, next) {
-	if (req.isAuthenticated()) return next();
-	res.redirect('/login');
+    if (req.isAuthenticated()) return next();
+    res.redirect('/login');
 }
 
 function isLoggedOut(req, res, next) {
-	if (!req.isAuthenticated()) return next();
-	res.redirect('/');
+    if (!req.isAuthenticated()) return next();
+    res.redirect('/');
 }
 
 // ROUTES
 app.get('/', (req, res) => {
-	res.render("index", { title: "Home" });
+    res.render("index", { title: "Home" });
 });
 
-app.get('/admin-dashboard', isLoggedIn, (req, res) => {
-	res.render("admin-dashboard");
-})
-
 app.get('/about', (req, res) => {
-	res.render("about");
+    res.render("about");
 })
 
 app.get('/learn-more', (req, res) => {
-	res.render("learnMore");
+    res.render("learnMore");
 })
 
 app.get('/priority-applicant', (req, res) => {
-	res.render("priorityApplicant");
+    res.render("priorityApplicant");
 })
 
 app.get('/resume-display', (req, res) => {
-	res.render("resumeDisplay");
+    res.render("resumeDisplay");
+})
+
+app.get('/success', (req, res) => {
+    res.render("success");
 })
 
 app.get('/login', isLoggedOut, (req, res) => {
-	const response = {
-		title: "Login",
-		error: req.query.error
-	}
+    const response = {
+        title: "Login",
+        error: req.query.error
+    }
 
-	res.render('login', response);
+    res.render('login', response);
 });
 
 app.post('/login', passport.authenticate('local', {
-	failureRedirect: '/login?error=true',
-	successRedirect: '/admin-dashboard',
+    failureRedirect: '/login?error=true',
+    successRedirect: '/admin-dashboard',
 }));
 
-app.get('/logout', function (req, res) {
-	req.logout();
-	res.redirect('/');
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
 });
 
 // bcrypt.genSalt(10, function (err, salt) {
@@ -134,16 +133,25 @@ app.get('/logout', function (req, res) {
 /****************************************************************************************************************************************************/
 
 var multer = require('multer');
-var fs =  require('fs');
-var path = require ('path');
+var fs = require('fs');
+var path = require('path');
 
 var imageSchema = new mongoose.Schema({
-    img:{
+    First_Name: String,
+    Last_Name: String,
+    Contact: String,
+    Date_of_Birth: Date,
+    Gender: String,
+    Email: String,
+    City: String,
+    State: String,
+    Pincode: String,
+    img: {
         data: String,
         contentType: String
     }
 });
-  
+
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads')
@@ -152,31 +160,30 @@ var storage = multer.diskStorage({
         cb(null, 'pdf' + '-' + Date.now() + '.pdf')
     }
 });
-  
+
 var upload = multer({ storage: storage });
 
-var fileUploadModel = new mongoose.model('Image', imageSchema);
+var candDetailsModel = new mongoose.model('candidate-details', imageSchema);
 
-app.get('/show-entries', (req, res) => {
-    fileUploadModel.find({}, (err, items) => {
+app.get('/admin-dashboard', isLoggedIn, (req, res) => {
+    candDetailsModel.find({}, (err, items) => {
         if (err) {
             console.log(err);
             res.status(500).send('An error occurred', err);
-        }
-        else {
-            res.render('showInputs', { items: items });
+        } else {
+            res.render('admin-dashboard', { items: items });
         }
     });
 });
 
-app.get('/get-hired', (req,res) =>{
+app.get('/get-hired', (req, res) => {
     res.render('profile');
 })
 
-app.get('/downloadFile/:reqSite', function (req, res) {
+app.get('/downloadFile/:reqSite', function(req, res) {
     var file = path.join(__dirname + '/uploads/' + req.params.reqSite);
 
-    res.download(file, function (err) {
+    res.download(file, function(err) {
         if (err) {
             console.log("Error");
             console.log(err);
@@ -184,30 +191,38 @@ app.get('/downloadFile/:reqSite', function (req, res) {
             console.log("Success");
         }
     });
- });
+});
 
 //Details Post Request
 app.post('/upload', upload.single('resume'), (req, res, next) => {
     // console.log(path.join(__dirname + '/uploads/' + req.file.filename + '.pdf'));
     var obj = {
+        First_Name: req.body.First_Name,
+        Last_Name: req.body.Last_Name,
+        Contact: req.body.Contact,
+        Date_of_Birth: req.body.Date_of_Birth,
+        Gender: req.body.Gender,
+        Email: req.body.Email,
+        City: req.body.City,
+        State: req.body.State,
+        Pincode: req.body.Pincode,
         img: {
             data: req.file.filename,
             contentType: 'application/pdf'
-        }  
+        }
     }
 
-    fileUploadModel.create(obj, (err, item) => {
+    candDetailsModel.create(obj, (err, item) => {
         if (err) {
             console.log(err);
-        }
-        else {
+        } else {
             item.save();
-            res.send('Registration Successfull');
+            res.render("success");
         }
     });
 });
 
 
 app.listen(3000, () => {
-	console.log("Listening on port 3000");
+    console.log("Listening on port 3000");
 });
